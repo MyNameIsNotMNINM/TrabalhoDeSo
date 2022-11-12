554eff3c02ae814814a974f776cafc769c000137
Rafael Pereira <rafael.pereira-1@syngenta.com>
2022-11-11 21:33:35 -0300

    CPU class and create process done.

--- core/systemManager.ts
+++ core/systemManager.ts
@@ -1,8 +1,31 @@
-
-interface Program {
-    pid: number,
+interface Process {
+    pid?: number,
     creationTime: number,
     executionTime: number,
     deadline: number,
     priority: number
 }
+
+enum SchedulingAlgs {
+    FIFO,
+    SJF,
+    RR,
+    EDF,
+}
+class CPU {
+    quantum: number;
+    schedulingAlg: SchedulingAlgs;
+    processQueue: Process[] = [];
+    processes: Process[] = [];
+    constructor(quantum:number = 2,sa: SchedulingAlgs){
+        this.quantum = quantum;
+        this.schedulingAlg = sa;
+    }
+
+    AddProcess(process: Process){
+        this.processes.push(process);
+    }
+
+}
+
+export default CPU;
