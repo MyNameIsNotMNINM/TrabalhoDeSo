4c91c72b6842cb6574bed70cc0b5f5349d4f7b8c
Douglas Silva <douglas.jesus@qintess.com>
2022-11-26 12:11:50 -0300

    In progress
    In progress

--- src/core/systemManager.ts
+++ src/core/systemManager.ts
@@ -6,20 +6,19 @@ interface Process {
     executionTime: number,
     deadline: number,
     priority: number,
-    clocksProcessed?: number
+    clocksProcessed: number
 }
 
 class CPU {
     quantum: number;
-    lastQuantum: number = 0;
+    quantumEnd: number = number.MIN_VALUE;
     currentClock: number = 0;
     overload: number;
     schedulingAlg: SchedulingAlgs;
     processQueue: Process[] = [];
     processes: Process[] = [];
     runningProcess: Process | null = null;
-    isCurrentlyOverloading: boolean = false;
-    overloadStart: number = Number.MAX_VALUE;
+    overloadEnd: number = Number.MIN_VALUE;
     static pid_count = 1;
    
     constructor(sa: SchedulingAlgs, quantum: number, overload: number){
@@ -36,29 +35,30 @@ class CPU {
     }
 
     Clock() {
+        this.spawnProcess();
+
         if(this.hasQuantumEnded() && Scheduler.algorithmHasTimeSharing(this.schedulingAlg)){
             if(this.runningProcess){
                 this.processQueue.unshift(this.runningProcess);
                 this.processQueue = Scheduler.sortProcesses(this.schedulingAlg, this.processQueue);
+                this.overloadEnd = this.currentClock + this.quantum;
             }
-            this.changeContext(this.PopNextProcess());
-            return;
         }
+        if (this.hasOverloadEnded() && !this.runningProcess)
+        {
+            this.changeContext(this.PopNextProcess());
+            this.runProcess();   
+        }
+        this.currentClock++;
+    }
+
+    private spawnProcess() {
         this.processes.forEach(element => {
             if (element.creationTime == this.currentClock){
                 this.processQueue.push(element);
                 this.processQueue = Scheduler.sortProcesses(this.schedulingAlg, this.processQueue);
             }
         });
-        if(this.runningProcess){
-            if(!this.runningProcess.clocksProcessed)
-                return;
-            this.runningProcess.clocksProcessed++;
-            if(this.runningProcess.clocksProcessed >= this.runningProcess.executionTime)
-                this.runningProcess = null;
-            return;
-        }
-        this.currentClock++;
     }
 
     getCPUContext(){
@@ -68,17 +68,29 @@ class CPU {
         }
     }
 
+    private runProcess() {
+        if (this.runningProcess) {
+            this.runningProcess.clocksProcessed++;
+            if (this.runningProcess.clocksProcessed >= this.runningProcess.executionTime)
+                this.runningProcess = null;
+        }
+    }
+
     private hasQuantumEnded(){
-        return this.currentClock - this.lastQuantum >= this.quantum;
+        return this.currentClock >= this.quantumEnd;
     }
 
     private changeContext(process: Process | undefined){
         this.runningProcess = process || null;
-
+        this.quantumEnd = this.currentClock + this.quantum;
     }
 
     private PopNextProcess(): Process | undefined{
         return this.processQueue.shift()
+    }
+
+    private hasOverloadEnded() {
+        return this.overloadEnd <= this.currentClock;
     }
 
     getOverloadProcess() {
