6418b515044d03a88cb328ee6386208723e491ec
Rafael Pereira <rafael.pereira-1@syngenta.com>
2022-11-29 01:12:32 -0300

    process creator ui

--- src/components/processCreator.tsx
+++ src/components/processCreator.tsx
@@ -19,10 +19,30 @@ interface ProcessCreatorDTO{
 }
 
 const ProcessCreator = (props: ProcessCreatorProps) => {
-  const [data, setData] = useState<ProcessCreatorDTO>();
+  const [data, setData] = useState<ProcessCreatorDTO>({
+    processes: [],
+    schedulingAlgorithm: SchedulingAlgs.FIFO,
+    quantum: 1,
+    overload: 1,
+  });
+
+  function modifyCPUData(modifiedData: {[Key: string]: any}){
+    let newData: any = {... data};
+    Object.entries(modifiedData).forEach(entry => {
+      newData[entry[0]] = entry[1];
+    });
+    setData(newData);
+  }
+
   return (
     <RightSideModal enabled={true}>
-
+      <input type="text" value={data.quantum}  onChange={
+        e => {
+          modifyCPUData(
+            {"quantum":parseFloat(e.target.value)|| 0}
+            )
+        }
+      }/>
     </RightSideModal>
   );
 };
